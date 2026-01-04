5d1c0b7546884ff0f59472981a41989e18cc37d0
zboz1 <zbozman231@gmail.com>
2026-01-04 14:40:47 -0800

    updated main.py and stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_book_text_count, count_characters
+from stats import get_book_text_count, count_characters, sort_dictionary
 
 def main():
     num = 0
@@ -8,8 +8,16 @@ def main():
         num += 1
     
     chars = count_characters(word_count)
+    sorted_list = sort_dictionary(chars)
     
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {num} total words")
-    print(f"{chars}")
+    print("--------- Character Count -------")
+    for i in sorted_list:
+        if i["char"].isalpha() == True:
+             print(f"{i["char"]}: {i["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,4 @@
-word_count = ""
 # Takes a relative file path and converts its contents into a useable string
-# 
 def get_book_text_count(filepath):
 
     with open(filepath) as f:
@@ -10,7 +8,8 @@ def get_book_text_count(filepath):
     
     return word_count
 
-# Converts an input string into a dictionary counting the number of times each character
+
+# Converts an input string into a dictionary counting the number of times each character appears
 def count_characters(string):
     character_count = {}
     book_string = ""
@@ -28,3 +27,20 @@ def count_characters(string):
             character_count[character] += 1
         
     return character_count
+
+def get_num_key(dict):
+    num_key = dict["num"]
+    return num_key
+
+def sort_dictionary(dictionary):
+    sorted_list = []
+    char_dict = {}
+
+    for i in dictionary:
+        char_dict["char"] = i
+        char_dict["num"] = dictionary[i]
+        sorted_list.append(char_dict)
+        char_dict = {}
+
+    sorted_list.sort(reverse=True, key=get_num_key)
+    return sorted_list
